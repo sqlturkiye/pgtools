1c7f60769024e82bdbc69562279ef5b6dc8dea4d
yusufkahveci@sqlturkiye.com <190586Gs>
2020-05-23 01:26:54 +0300

    pg_export_v1

--- pg_export.py
+++ pg_export.py
@@ -1,4 +1,16 @@
-# PostgreSQL Connection Check
+# Bu programın amacı ,
+# İstediğiniz herhangi bir postgreSQL server 'ında istenilen veritabanında sorgu ve tabloyu istediğiniz path altında export unu csv olarak çıkarmak.
+# Programda Örnek giriş bilgileri aşağıdadır.
+# Veritabanı Adını Giriniz : db_name
+# Kullanıcı Adını Giriniz : user
+# Şifrenizi Giriniz : Pass
+# Host Bilginizi Giriniz : IP /hostname
+# Port : 5432
+# Export edilecek query ya da tabloyu giriniz : test
+# Path bilgisini giriniz : /tmp/demo99.csv
+# Delimiter : ,
+# NOT : Export edilecek query olacak ise () içerisinde query bilgisini girmelisiniz.Örnek :  (SELECT name,surname FROM test2)
+
 db_name= input("Veritabanı Adını Giriniz : ")
 user_name = input("Kullanıcı Adını Giriniz : ")
 passwd = input("Şifrenizi Giriniz : ")
@@ -23,15 +35,3 @@ print(query)
 cursor.execute(query)
 con.commit()
 
-# Bu programın amacı , İstediğiniz herhangi bir postgreSQL server 'ında istenilen veritabanında sorgu ve tabloyu istediğiniz path altında export unu csv olarak çıkarmak.
-# Programda Örnek giriş bilgileri aşağıdadır.
-# Veritabanı Adını Giriniz : db_name
-# Kullanıcı Adını Giriniz : user
-# Şifrenizi Giriniz : Pass
-# Host Bilginizi Giriniz : IP /hostname
-# Port : 5432
-# Export edilecek query ya da tabloyu giriniz : test
-# Path bilgisini giriniz : /tmp/demo99.csv
-# Delimiter : ,
-
-# NOT : Export edilecek query olacak ise () içerisinde query bilgisini girmelisiniz.Örnek :  (SELECT name,surname FROM test2)
