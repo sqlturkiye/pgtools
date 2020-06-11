46cf1b2f25d680ce8babb5b88418961fac20ce03
yusufkahveci@sqlturkiye.com <190586Gs>
2020-06-11 16:38:25 +0300

    pg_get_user_db_connection_count_v1

---
+++ pg_get_user_db_connection_count.py
@@ -0,0 +1,31 @@
+# Bu programın amacı ,
+# İstediğiniz herhangi bir postgreSQL server 'ında db ve kullanıcı bazlı connection sayılarını vermektedir.
+# Programda Örnek giriş bilgileri aşağıdadır.
+# Veritabanı Adını Giriniz : db_name
+# Kullanıcı Adını Giriniz : user
+# Şifrenizi Giriniz : Pass
+# Host Bilginizi Giriniz : IP /hostname
+# Port : 5432
+
+db_name = input("Veritabanı Adını Giriniz : ")
+user_name = input("Kullanıcı Adını Giriniz : ")
+passwd = input("Şifrenizi Giriniz : ")
+host_ = input("Host Bilginizi Giriniz : ")
+prt = input("Port : ")
+import psycopg2
+
+con = psycopg2.connect(database=db_name, user=user_name, password=passwd, host=host_, port=prt)
+import pandas as psql
+
+print("Connecting to Database")
+
+con.autocommit = True
+cursor = con.cursor()
+
+q1 = "SELECT datname,usename, count(*) FROM pg_stat_activity GROUP BY 1, 2 ORDER BY 1, 2;"
+query = q1
+df = psql.read_sql(query, con)
+print(df)
+
+con.commit()
+con.close()
