5381214becdd79bac2f469e3feb19bc5f28ac631
yusufkahveci@sqlturkiye.com <190586Gs>
2020-06-11 16:22:05 +0300

    pg_get_setting_tcp_v1

---
+++ pg_get_setting_tcp.py
@@ -0,0 +1,31 @@
+# Bu programın amacı ,
+# İstediğiniz herhangi bir postgreSQL server 'ında ki tcp ayarlarını görmenizi sağlar.
+# Programda Örnek giriş bilgileri aşağıdadır.
+# Veritabanı Adını Giriniz : db_name
+# Kullanıcı Adını Giriniz : user
+# Şifrenizi Giriniz : Pass
+# Host Bilginizi Giriniz : IP /hostname
+# Port : 5432
+
+db_name = input("Veritabanı Adını Giriniz : ")
+user_name = input("Kullanıcı Adını Giriniz : ")
+passwd = input("Şifrenizi Giriniz : ")
+host_ = input("Host Bilginizi Giriniz : ")
+prt = input("Port : ")
+import psycopg2
+
+con = psycopg2.connect(database=db_name, user=user_name, password=passwd, host=host_, port=prt)
+import pandas as psql
+
+print("Connecting to Database")
+
+con.autocommit = True
+cursor = con.cursor()
+
+q1 = "SELECT name, setting, short_desc FROM pg_settings WHERE name LIKE " + "'tcp%'" + ";"
+query = q1
+df = psql.read_sql(query, con)
+print(df)
+
+con.commit()
+con.close()
